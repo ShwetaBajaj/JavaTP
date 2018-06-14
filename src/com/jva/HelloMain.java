abfb4a5f71e437fe42e37036516da83ece2f67bf
ACER <ACER@DESKTOP-LSKOOU6>
2018-06-14 17:03:38 +0530

    new files added

---
+++ src/com/jva/HelloMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jva;
+
+/**
+ * @author Shweta
+ *
+ */
+public class HelloMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is test of GitHub.com");
+
+	}
+
+}
