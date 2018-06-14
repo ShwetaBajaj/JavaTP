e30016a43a279d198eefe57cdec38475c1084651
ACER <ACER@DESKTOP-LSKOOU6>
2018-06-14 17:44:51 +0530

    updating hello class.

--- src/com/jva/HelloMain.java
+++ src/com/jva/HelloMain.java
@@ -14,6 +14,7 @@ public class HelloMain {
 	 */
 	public static void main(String[] args) {
 		System.out.println("This is test of GitHub.com123");
+		System.out.println("This is second line.");
 
 	}
 
